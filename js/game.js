5ec9feb8b84e786124a4c2419e88efdadce4c946
Thienan Nguyen <thienan.nguyen@ousd.us>
2019-02-28 01:52:47 +0000

    Adding images, added 2 levels.

--- js/game.js
+++ js/game.js
@@ -2,33 +2,38 @@
 
 var game = {
     music: "98_Lost_Mine.mp3",
-    background_image: "intro-bg.jpg",
+    background_image: "town.jpg",
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You over hear conversations and whispers. The people tell of endless wealth to those who can find the hidden treasure. But you were unable to decipher if the treasure was located in the cave or the cove.",
             choices: [
                 {
-                    text: "Enter the cave",
+                    text: "Travel to the Cave",
                     nextLevel: "cave",
                 },
 
                 {
-                    text: "Keep on moving",
+                    text: "Travel to the Cove",
                     nextLevel: "field",
                 },
             ]
         },
 
         cave: {
-            background_image: "fire.gif",
+            background_image: "cave.jpg",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "You stand before the mouth of the cavern. Light is consumed by the shadow of the depths. You enter slowly.",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "Explore",
+                    nextLevel: "explore",
                 },
+                
+                {
+                    text: "Encounter",
+                    nextLevel: "encounter",
+                }
             ]
         },
 
